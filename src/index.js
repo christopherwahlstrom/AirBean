ccc4f484c954a249dd04b3b65cafff32c4b4077e
Monica Björk <m.bjork95@gmail.com>
2022-05-02 14:32:19 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
-import todoReducer from './reducers/todoReducer';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 
@@ -10,7 +9,6 @@ const container = document.getElementById('root');
 const root = createRoot(container);
 
 const store = createStore(
-  todoReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
