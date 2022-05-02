70377726923a70fc45064abd9ec4e5e37c40c9d5
Monica Björk <m.bjork95@gmail.com>
2022-05-02 16:00:31 +0200

    Update Menu.js

--- src/views/Menu.js
+++ src/views/Menu.js
@@ -1,11 +1,9 @@
 import Header from '../components/Header';
 import MenuItem from '../components/MenuItem';
-import TopBackground from '../graphics/._graphics-footer.svg'
 
 function Menu() {
-    console.log(TopBackground);
     return (
-        <article className="menu">
+        <article className="menu leafyBackground">
             <Header />
             <h1>Meny</h1>
             <MenuItem/>
