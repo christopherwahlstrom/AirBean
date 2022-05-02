5243fa96a9fd074391229e00a9bb7e345fa306ec
Monica Björk <m.bjork95@gmail.com>
2022-05-02 14:29:53 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
+import todoReducer from './reducers/todoReducer';
+import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import store from './store';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+const store = createStore(
+  todoReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
 root.render(
   <Provider store={store}>
     <App />
