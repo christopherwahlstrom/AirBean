df9eb458d63285455596c2512b433695c3dee042
Hampus83 <jhmacgyver@gmail.com>
2022-05-02 23:00:53 +0200

    NavMenu html, css and nav-links finished

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -1,12 +1,25 @@
 import './NavMenu.css';
 
-import buttonImg from '../graphics/close.svg'
+import buttonImg from '../graphics/close.svg';
+
+import { Link } from 'react-router-dom';
 
 function NavMenu() {
     return (
-        <section className="navmenu">
-            <input className='close-btn' type="image" src={ buttonImg } />
-        </section>
+        <article className="navmenu">
+            <section className="btn-container">
+                <input className='close-btn' type="image" src={ buttonImg } />
+            </section>
+            <nav className="links">
+                <Link className='link' to="/menu">Meny</Link>
+                <hr />
+                <Link className='link' to="/about">Vårt kaffe</Link>
+                <hr />
+                <Link className='link' to="/order">Orderstatus</Link>
+                <hr />
+            </nav>
+            
+        </article>
     );
 }
 
