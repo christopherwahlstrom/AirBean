16acdd823b82fd8d22991b52ddb30818cede98e3
RickyJohansson <Rickjoh91@hotmail.com>
2022-05-02 23:06:14 +0200

    view order complete
    View: Order - done!

--- src/views/Order.js
+++ src/views/Order.js
@@ -1,12 +1,14 @@
+import './Order.css';
+import drone from '../graphics/drone.svg';
 
 
 function Order() {
     return (
         <section className='order-page'>
-            <p>Ordernummer<span></span></p>
-            <img></img>
-            <h1>Din beställning är på väg!</h1>
-            <span></span><p>minuter</p>
+            <p>Ordernummer<span className='order-number'></span></p>
+            <img src={ drone }></img>
+            <h1>Din beställning <br /> är på väg!</h1>
+            <span className='time'></span><p className='minutes'>minuter</p>
             <button>Ok, cool!</button>
         </section>
     );
