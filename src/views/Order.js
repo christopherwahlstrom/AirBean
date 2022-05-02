6e4023b86dae76b093125b72502821dbd1e5e0e9
RickyJohansson <Rickjoh91@hotmail.com>
2022-05-02 15:07:15 +0200

    Update Order.js

--- src/views/Order.js
+++ src/views/Order.js
@@ -1,10 +1,12 @@
 function Order() {
     return (
-        <p>Ordernummer<span></span></p>
-        <img></img>
-        <h1>Din beställning är på väg!</h1>
-        <span></span><p>minuter</p>
-        <button>Ok, cool!</button>
+        <section>
+            <p>Ordernummer<span></span></p>
+            <img></img>
+            <h1>Din beställning är på väg!</h1>
+            <span></span><p>minuter</p>
+            <button>Ok, cool!</button>
+        </section>
     );
 }
 
