4d0be22ef5ddffb9ab0422c90e08d0f779acca80
Hampus83 <jhmacgyver@gmail.com>
2022-05-02 14:58:50 +0200

    added routes and paths

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
 
+import { Routes, Route } from 'react-router-dom';
+
+import Landing from './views/Landing';
+import Menu from './views/Menu';
+import About from './views/About';
+import Order from './views/Order';
+
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path='/' element={ <Landing /> } />
+        <Route path='/menu' element={ <Menu /> }/>
+        <Route path='/about' element={ <About /> }/>
+        <Route path='/order' element={ <Order /> }/>
+      </Routes>
     </div>
   );
 }
