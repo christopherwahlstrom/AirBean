451d8c860155fc864749fbcc266d783f21a64b56
Hampus83 <jhmacgyver@gmail.com>
2022-05-03 10:39:57 +0200

    started work with Cart.js

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import { createRoot } from 'react-dom/client';
 
 import './index.css';
 import App from './App';
-import { createStore } from 'redux';
+
 import { Provider } from 'react-redux';
 
 import { createStore } from 'redux';
-import { createRoot } from 'react-dom/client'
+
 import allReducers from './reducers';
 import { BrowserRouter } from 'react-router-dom';
 
