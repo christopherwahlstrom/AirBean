815c1a444ae1a1d3dbf33fe9efa00035859b4778
Monica Björk <m.bjork95@gmail.com>
2022-05-03 10:08:19 +0200

    adding props

--- src/views/Menu.js
+++ src/views/Menu.js
@@ -3,14 +3,16 @@ import './Menu.css';
 import Header from '../components/Header';
 import MenuItem from '../components/MenuItem';
 
-function Menu() {
+async function Menu(props) {
+    /*const response = await fetch('https://my-json-server.typicode.com/zocom-christoffer-wallenberg/airbean/menu');
+    const data = await response.json();
+    console.log(data);
+*/
     return (
         <article className="menu leafyBackground">
                 <Header />
                 <h1 className="menu--title">Meny</h1>
-                <MenuItem/>
-                <MenuItem/>
-                <MenuItem/>
+                <MenuItem title="kaffe" price="10kr" desc="en vanlig jävla kaffe" />
         </article>
     );
 }
