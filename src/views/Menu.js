eac9dc91a5bdb6cb05fe940d72f672cb2f7ada19
Monica Björk <m.bjork95@gmail.com>
2022-05-03 10:32:10 +0200

    working on props

--- src/views/Menu.js
+++ src/views/Menu.js
@@ -2,17 +2,24 @@ import './Menu.css';
 
 import Header from '../components/Header';
 import MenuItem from '../components/MenuItem';
+import { useState } from 'react';
 
-async function Menu(props) {
+async function Menu() {
+    const [ title, setTitle ] = useState("Kaffe");
+    const [ price, setPrice ] = useState(50);
+    const [ desc, setDesc ] = useState("En vanlig jävla kaffe");
+
     /*const response = await fetch('https://my-json-server.typicode.com/zocom-christoffer-wallenberg/airbean/menu');
     const data = await response.json();
     console.log(data);
+
+    
 */
     return (
         <article className="menu leafyBackground">
                 <Header />
                 <h1 className="menu--title">Meny</h1>
-                <MenuItem title="kaffe" price="10kr" desc="en vanlig jävla kaffe" />
+                <MenuItem title={ title } price={ price + "kr"} desc={ desc } />
         </article>
     );
 }
