96e8fb48098a9709df41c0b742dad8bd4ad8febe
Monica Björk <m.bjork95@gmail.com>
2022-05-03 13:46:46 +0200

    header styled

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,20 @@
+import './Header.css';
+
+import Cart from './Cart';
+import NavMenu from './NavMenu';
+import shoppingBag from '../graphics/bag.svg';
+import navicon from '../graphics/navicon.svg';
+
 function Header() {
     return (
-        <section>
-            header
+        <section className="header">
+            <section className="header--iconWrapper iconWrapper--nav" onClick={ NavMenu }>
+                <img src={ navicon } alt="shopping bag" className="header--icon" />
+            </section>
+
+            <section className="header--iconWrapper iconWrapper--cart" onClick={ Cart } >
+                <img src={ shoppingBag } alt="shopping bag" className="header--icon" />
+            </section>
         </section>
     );
 }
