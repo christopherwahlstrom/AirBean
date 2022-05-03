42dead3874c8e87bb4d933c3bb70ac474ef49691
Christopher Wahlström <83230387+christopherwahlstrom@users.noreply.github.com>
2022-05-03 10:17:22 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-
 import './index.css';
 import App from './App';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-
-import { createStore } from 'redux';
-import { createRoot } from 'react-dom/client'
 import allReducers from './reducers';
 import { BrowserRouter } from 'react-router-dom';
 
