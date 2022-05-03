c5d3a4eadf212d011cbe4680be896ee9d7f8be72
Christopher Wahlström <83230387+christopherwahlstrom@users.noreply.github.com>
2022-05-03 14:55:18 +0200

    Skapat actions

---
+++ src/actions/index.js
@@ -0,0 +1,43 @@
+export const increment = () => {
+    return {
+        type: 'INCREMENT'
+    };
+};
+
+export const decrement = () => {
+    return {
+        type: 'DECREMENT'
+    };
+};
+
+export const addPricePoint = (value) => {
+    return {
+        type: 'PRICE',
+        price: value
+    };
+};
+
+export const clearPrice = () => {
+    return {
+        type: 'CLEAR_PRICE'
+    };
+};
+
+export const addItem = (item) => {
+    return {
+        type: 'ADD_ITEM',
+        object: item
+    };
+};
+
+export const clearItem = () => {
+    return {
+        type: 'CLEAR'
+    };
+};
+
+export const timer = () => {
+    return {
+        type: 'START'
+    }
+}
