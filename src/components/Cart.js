ead93cbddca214f9206306654ae0e5f81f603bcc
Hampus83 <jhmacgyver@gmail.com>
2022-05-03 12:26:20 +0200

    Cart HTML & CSS finished

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -5,8 +5,13 @@ function Cart() {
         <section className='cart-wrapper'>
             <article className='cart'>
                 <h1>Din Beställning</h1>
-                <h2>Total</h2>
-                <button className='cart-btn'></button>
+                <article className='sum-container'>
+                    <h2 className='total'>Total</h2>
+                    <section className='dots'></section>
+                    <h2 className='sum'>98 kr</h2>
+                </article>
+                <p>Inkl. moms + drönarleverans</p>
+                <button className='cart-btn'>Take my money!</button>
             </article>
         </section>
     );
