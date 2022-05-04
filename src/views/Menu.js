468e79516a9d2b73fd93d09beeab191d802a6382
RickyJohansson <Rickjoh91@hotmail.com>
2022-05-04 09:12:13 +0200

    update API

--- src/views/Menu.js
+++ src/views/Menu.js
@@ -8,16 +8,6 @@ function Menu(props) {
 
     const { menu } = props;
 
-    useEffect(() => {
-        async function getMenu() {
-            const response = await fetch('https://my-json-server.typicode.com/zocom-christoffer-wallenberg/airbean/menu');
-            const data = await response.json();
-            setMenu(data);
-        }
-        getMenu();
-    }, []);
-
-
     const menuItems = menu.map((menuItem, index) => {
         return <MenuItem title={ menuItem.title } price={ menuItem.price } desc={ menuItem.desc } key={ index }/>
     });
