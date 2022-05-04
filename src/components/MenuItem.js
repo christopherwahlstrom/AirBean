e35e3f9b64dca6d3b854c37d363781029686b7f1
Monica Björk <m.bjork95@gmail.com>
2022-05-04 09:13:20 +0200

    changed add button

--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -1,11 +1,12 @@
 import './MenuItem.css';
+import AddIcon from '../graphics/add.svg';
 
 function MenuItem(props) {
     const { title, desc, price } = props;
 
     return (
         <article className="menuItem">
-            <button className="menuItem--button">+</button>
+            <img src={AddIcon} alt="add to cart" />
             <section>
                 <h2 className="menuItem--title">{ title }<span className="menuItem--price">{price}</span></h2>
                 <p className="menuItem--description">{desc}</p>
