1941c322c67ed88db405cba4cc2c0033da6173fb
RickyJohansson <Rickjoh91@hotmail.com>
2022-05-04 09:01:32 +0200

    updated API to App

--- src/views/Menu.js
+++ src/views/Menu.js
@@ -2,10 +2,11 @@ import './Menu.css';
 
 import Header from '../components/Header';
 import MenuItem from '../components/MenuItem';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
-function Menu() {
-    const [menu, setMenu] = useState([]);
+function Menu(props) {
+
+    const { menu } = props;
 
     //Härifrån och ner till och med rad 24 försökte jag mig på liknande som vi gjorde tillsammans med Monica igår, 
     //men för att få tag i "visible/visibleCart" behöver props hoppa fler än 1 steg, så då kanske det är bäst att 
@@ -22,15 +23,6 @@ function Menu() {
     }
 
     // Här slutar den ofärdiga koden som nämns på rad 10 //HE
-
-    useEffect(() => {
-        async function getMenu() {
-            const response = await fetch('https://my-json-server.typicode.com/zocom-christoffer-wallenberg/airbean/menu');
-            const data = await response.json();
-            setMenu(data);
-        }
-        getMenu();
-    }, []);
 
     const menuItems = menu.map((menuItem, index) => {
         return <MenuItem title={ menuItem.title } price={ menuItem.price } desc={ menuItem.desc } key={ index }/>
