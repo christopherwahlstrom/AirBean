732ac85f83b5bc560569c136c09d902e3942c33a
Monica Björk <m.bjork95@gmail.com>
2022-05-05 14:49:56 +0200

    logging

--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -16,7 +16,6 @@ function MenuItem(props) {
 
     function addToCart() {
         dispatch(itemAction({ title: title, price: price, quant: quant }));
-        console.log('tjo!');
         console.log(title);
         console.log(quant);
         // const cartItemsCopy = [...cartItems.item]
